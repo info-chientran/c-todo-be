4fded952f0052fde361f07da22e259f0de658609
Tran Van Chien <info.chientran@gmail.com>
2025-02-27 09:58:14 +0700

    [enh] Init project

---
+++ src/main/java/com/c_todo_be/CTodoBeApplication.java
@@ -0,0 +1,13 @@
+package com.c_todo_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CTodoBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CTodoBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/c_todo_be/CTodoBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.c_todo_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CTodoBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
