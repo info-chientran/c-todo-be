acacb6cc2ad7dc592679b70b48ebc5d80a0aae53
Tran Van Chien <info.chientran@gmail.com>
2025-03-04 08:54:31 +0700

    [feat] CRUD todo

---
+++ src/main/java/com/c_todo_be/entity/User.java
@@ -0,0 +1,28 @@
+package com.c_todo_be.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String fullName;
+
+    @Column(nullable = true)
+    private String avatar;
+}
---
+++ src/main/java/com/c_todo_be/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.c_todo_be.repository;
+
+import com.c_todo_be.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
